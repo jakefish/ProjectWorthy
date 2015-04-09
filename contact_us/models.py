a4ec68e5538205ae1bdd6b6782d060a8097147d9
fishb1jw <fishb1jw@cmich.edu>
2015-04-09 15:38:51 -0400

    Add fields to models

--- contact_us/models.py
+++ contact_us/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 class ContactUsSubmission(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=75)
+    website = models.URLField(max_length=200)
+    comment = models.TextField()
+    question_response = models.TextField()
+
+    
