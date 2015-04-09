093e2b17ffd0b5dc8cebb42ba19a48aaddf9826f
fishb1jw <fishb1jw@cmich.edu>
2015-04-09 15:41:09 -0400

    Create rough Reply model

--- contact_us/models.py
+++ contact_us/models.py
@@ -8,4 +8,9 @@ class ContactUsSubmission(models.Model):
     comment = models.TextField()
     question_response = models.TextField()
 
-    
+
+class Reply(models.Model):
+    comment = models.TextField()
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=75)
+    website = models.URLField(max_length=200)
