803219e51358427b0857274545c116d6994c28ad
fishb1jw <fishb1jw@cmich.edu>
2015-04-13 15:40:33 -0400

    Add more admin functionality

--- wod/admin.py
+++ wod/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
 from .models import Wod
-# Register your models here.
-admin.site.register(Wod)
+
+class WodAdmin(admin.ModelAdmin):
+#    fields = ['warm_up','weightlifting','metcon','strength_accessory']
+    date_hierarchy = 'date'
+    fieldsets = [
+        ('Warm Up',               {'fields': ['warm_up'], 'classes':['collapse']}),
+        ('Weightlifting',                {'fields': ['weightlifting']}),
+        ('Metcon', {'fields': ['metcon']}),
+        ('Strength Accessory', {'fields':['strength_accessory']}),
+    ]
+
+admin.site.register(Wod, WodAdmin)
