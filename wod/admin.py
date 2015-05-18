bed69d57ce702314b5504e6117a80f06f7448031
fishb1jw <fishb1jw@cmich.edu>
2015-05-18 08:01:50 -0400

    Update admin

--- wod/admin.py
+++ wod/admin.py
@@ -2,14 +2,23 @@ from django.contrib import admin
 
 from .models import Wod
 
+
+class WodInline(admin.TabularInline):
+    model = Wod
+
 class WodAdmin(admin.ModelAdmin):
 #    fields = ['warm_up','weightlifting','metcon','strength_accessory']
+
+
+
     date_hierarchy = 'date'
     fieldsets = [
-        ('Warm Up',               {'fields': ['warm_up'], 'classes':['collapse']}),
+        ('Warm Up',               {'fields': ['warm_up']}),
         ('Weightlifting',                {'fields': ['weightlifting']}),
         ('Metcon', {'fields': ['metcon']}),
         ('Strength Accessory', {'fields':['strength_accessory']}),
     ]
+
+    list_display = ('date',)
 
 admin.site.register(Wod, WodAdmin)
