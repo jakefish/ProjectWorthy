6a20ea2d0b3dfafbe917dcbd5d2c2f81fac0ad73
fishb1jw <fishb1jw@cmich.edu>
2015-06-04 15:12:52 -0400

    Update README.md file and remove unused variable from view

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ProjectWorthy
+This project is more of a project for fun, but is intended for practical use by
+the crossfit worthy gym.
--- wod/views.py
+++ wod/views.py
@@ -13,7 +13,5 @@ def wod_index(request):
     return render(request, "wod/wod_archive.html", context)
 
 def wod_details(request, wod_id):
-    response = "You're looking at the results of wod #."
     wod = Wod.objects.get(id=wod_id)
-
     return render(request, "wod/wod_page.html", {"wod": wod} )
