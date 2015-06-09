2e986e07bd470703f0203c8e39ae1e6e683781a4
fishb1jw <fishb1jw@cmich.edu>
2015-06-09 10:00:45 -0400

    Add an abstract base class model that provides self-updating "created" and     "modeified" fields.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+
+    """
+    An abstract base class model that provides
+    self-updating ``created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
