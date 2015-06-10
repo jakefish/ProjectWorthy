dad05b36c6895bf446ef3e7430bd6d745cbc5923
fishb1jw <fishb1jw@cmich.edu>
2015-06-10 09:13:34 -0400

    Remove url(s) in the wod app
    Changes were made to urls in the wod app that were suppose to be made in the
       urls for the core app. The urls for the wod app have been changed to their
       original state.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
+
 class CoreView(TemplateView):
 
     template_name = 'site_base.html'
-    
--- wod/urls.py
+++ wod/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.CoreView.as_view(), name='core_view'),
+    url(r'^$', views.wod_index, name='wod_index'),
+    url(r'^(?P<wod_id>[0-9]+)/$', views.wod_details, name='wod_details'),
 
 ]
