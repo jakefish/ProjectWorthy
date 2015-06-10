3187fb979c6194d4746fabd7720c2b218d0c3b61
fishb1jw <fishb1jw@cmich.edu>
2015-06-10 08:56:02 -0400

    Provide start of Class based views and urls
    ==> This is a skeleton not ready even for a development server

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class CoreView(TemplateView):
+
+    template_name = 'site_base.html'
+    
