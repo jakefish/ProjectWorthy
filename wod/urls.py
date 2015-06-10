09a6b9c8d02134f7daa469747b49d6b38b37ef4c
fishb1jw <fishb1jw@cmich.edu>
2015-06-10 09:09:18 -0400

    Create url to link to core view, also remove unnecessary urls

--- wod/urls.py
+++ wod/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.wod_index, name='wod_index'),
-    url(r'^(?P<wod_id>[0-9]+)/$', views.wod_details, name='wod_details'),
+    url(r'^$', views.CoreView.as_view(), name='core_view'),
 
 ]
