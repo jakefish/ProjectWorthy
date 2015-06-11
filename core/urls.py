502b734f05fcc258ef6575e74b15dfea70e19735
fishb1jw <fishb1jw@cmich.edu>
2015-06-11 14:43:14 -0400

    Link stylesheet to site_base.html

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.CoreView.as_view(), name='base_view'),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
