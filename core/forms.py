d92fa2c53874f1e41e80838ba826c1e964dae5ed
fishb1jw <fishb1jw@cmich.edu>
2016-03-26 13:32:25 -0400

    Add more fields to user form

--- core/forms.py
+++ core/forms.py
@@ -6,6 +6,5 @@ from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
 
     class Meta:
-        
         model = User
-        fields = ('email','username', 'password')
+        fields = ('email','username', 'password', 'first_name', 'last_name',)
